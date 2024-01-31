f2d746ca71b6c12282107ba92b6e3ddeda8e67e4
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-01-31 21:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, Response
-import socketserver
-import http.server
-import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup  # Assuming you have BeautifulSoup installed
 
 app = Flask(__name__)
 
-@app.route('/')
-def proxy():
-    url = request.args.get('url', '')
+@app.route('/<path:url>')
+def proxy(url):
     try:
-        with urllib.request.urlopen(url) as response:
-            content_type = response.headers.get('Content-Type', 'text/plain')
-            return Response(response.read(), content_type=content_type)
+        response = urlopen(url)
+        content_type = response.getheader('Content-Type')
+        html_content = response.read()
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Update links in the HTML content to include the full external URL
+        for a_tag in soup.find_all('a', href=True):
+            a_tag['href'] = f'{url}/{a_tag["href"]}'
+
+        # Update the HTML content
+        updated_html = str(soup)
+
+        return Response(updated_html, content_type=content_type)
     except Exception as e:
         return str(e)
 
