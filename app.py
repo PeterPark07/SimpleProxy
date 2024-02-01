5bb1d4fc8c3aa88191ff24bc7baa55b131135494
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 21:35:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,20 @@ def proxy(url):
     except Exception as e:
         return str(e)
 
+@app.route('/source/<path:url>')
+def source(url):
+    try:
+        response = urlopen(url)
+        content_type = response.getheader('Content-Type')
+        html_content = response.read()
+
+        # Pretty format the HTML source
+        pretty_html = pretty_format_html(html_content)
+        formatted_html = f'<pre>{pretty_html}</pre>'
+
+        return Response(formatted_html, content_type='text/plain')
+    except Exception as e:
+        return str(e)
+
 if __name__ == '__main__':
     app.run(debug=True)
